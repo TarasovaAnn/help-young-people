09fceeafd0a834b1e7dc8446981b7523d637d9fe
TarasovaAnn <tarasovaanna05009@gmail.com>
2025-11-11 21:50:57 +0300

    страница уборки и кнопка "назад"

--- app/src/main/java/com/example/indproject/Cleaning.kt
+++ app/src/main/java/com/example/indproject/Cleaning.kt
@@ -8,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class Cleaning : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        var button: Button = findViewById(R.id.back_btn)
+        enableEdgeToEdge()
+        var button: Button = findViewById(R.id.back_btn1)
         button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
