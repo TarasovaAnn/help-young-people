6b541c1b53bc71d7624548851b562b644eb381a4
TarasovaAnn <tarasovaanna05009@gmail.com>
2025-11-14 19:25:54 +0300

    перехолд через текст. коммит перед обновой

--- app/src/main/java/com/example/indproject/MainActivity.kt
+++ app/src/main/java/com/example/indproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.indproject
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,11 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
         }
+        val clickableSpan = findViewById<TextView>(R.id.text1)
+        clickableSpan.setOnClickListener {
+            val intent= Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
         var button2: Button = findViewById(R.id.btn2)
         button2.setOnClickListener {
             val intent = Intent(this, CleaningActivity::class.java)
@@ -26,6 +32,26 @@ class MainActivity : AppCompatActivity() {
         var button4: Button = findViewById(R.id.btn4)
         button4.setOnClickListener {
             val intent = Intent(this, TechnActivity::class.java)
+            startActivity(intent)
+        }
+        var button5: Button = findViewById(R.id.btn5)
+        button5.setOnClickListener {
+            val intent = Intent(this, GkxActivity::class.java)
+            startActivity(intent)
+        }
+        val clickableSpan2 = findViewById<TextView>(R.id.text2)
+        clickableSpan2.setOnClickListener {
+            val intent= Intent(this, CleaningActivity::class.java)
+            startActivity(intent)
+        }
+        val clickableSpan3 = findViewById<TextView>(R.id.text3)
+        clickableSpan3.setOnClickListener {
+            val intent= Intent(this, TechnActivity::class.java)
+            startActivity(intent)
+        }
+        val clickableSpan4 = findViewById<TextView>(R.id.text4)
+        clickableSpan4.setOnClickListener {
+            val intent= Intent(this, GkxActivity::class.java)
             startActivity(intent)
         }
     }
