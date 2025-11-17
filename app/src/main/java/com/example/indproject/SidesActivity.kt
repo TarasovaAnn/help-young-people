ad0072244174f9294bea47ff32cad3bcc21ecdda
TarasovaAnn <tarasovaanna05009@gmail.com>
2025-11-17 22:50:47 +0300

    иконки

---
+++ app/src/main/java/com/example/indproject/SidesActivity.kt
@@ -0,0 +1,22 @@
+package com.example.indproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SidesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sides)
+        var button: Button = findViewById(R.id.back_btn8)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
